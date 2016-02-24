30697ffa8faac1864f815f84299f07cd11a474f9
kokiddp <kokiddp@users.noreply.github.com>
2016-02-24 16:54:30 +0100

    Initial commit

---
+++ setup_and_blink.py
@@ -0,0 +1,19 @@
+from pyGreeney import pyGreeney
+import time
+
+b = pyGreeney('/dev/ttyACM0')
+pin = 13
+day = 1
+night = 1
+
+b.output([pin], day, night)
+
+b.setHigh(pin)
+time.sleep(10)
+print b.getState(pin)
+b.setLow(pin)
+print b.getState(pin)
+time.sleep(10)
+
+b.close()
+
